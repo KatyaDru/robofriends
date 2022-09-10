fd7e448151ad00223e2b95476e38e18761c7379e
KatyaDru <kdrusakov@gmail.com>
2022-09-10 14:01:16 +0300

    hello

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ const About = () => {
 			<img src='robots/headerRobot.png' alt='robot' />
 			<div className='dib pa3 ma3 ba br3'>
 				<p>
-				To make my first React app, I created some cute robots using Midjorney AI.
+				Hello! To make my first React app, I created some cute robots using Midjorney AI.
 				You can see the source code following <a href='https://github.com/KatyaDru/robofriends' rel='noreferrer' target='_blank' className='link dark-green dim'>this link</a>.
 				Midjorney is a Discord-bot that imagine pictures (in this case a robots) using custom keywords you can send to the bot.
 				</p>
